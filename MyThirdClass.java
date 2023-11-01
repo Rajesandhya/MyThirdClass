c2dc9ceddcb13638a866f3b56be046b1fee72536
Rajesh Limbachiya <rajesandhya_143@yahoo.co.in>
2023-11-01 21:56:06 +0000

    MyThirdClass

---
+++ MyThirdClass.java
@@ -0,0 +1,13 @@
+package mythirdpackage;
+
+public class MyThirdClass {
+
+    public static void main ( String[] args) {
+
+        System.out.println( "What is manual testing?");
+
+        System.out.println( "What are the phases of SDLC life cycle?");
+
+        System.out.println( " What are the phases of STLC life cycle?");
+    }
+}
